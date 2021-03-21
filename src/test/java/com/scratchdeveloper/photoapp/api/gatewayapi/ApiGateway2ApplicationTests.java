6a673eb1b9dbdfc7874be116d078f0533cd73a17
praveen <praveen.kumar02@hotmail.com>
2021-03-21 22:58:45 +0530

    First commit

---
+++ src/test/java/com/scratchdeveloper/photoapp/api/gatewayapi/ApiGateway2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scratchdeveloper.photoapp.api.gatewayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGateway2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
